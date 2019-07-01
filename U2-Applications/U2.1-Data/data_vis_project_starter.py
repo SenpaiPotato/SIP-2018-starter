0f1b541ca44d2dace8b04bf222fbcaa80cd70edf
SenpaiPotato <minrulee8@gmail.com>
2019-07-01 18:10:03 -0400

    cool data project

--- U2-Applications/U2.1-Data/data_vis_project_starter.py
+++ U2-Applications/U2.1-Data/data_vis_project_starter.py
@@ -13,8 +13,41 @@ tweetFile = open("TwitterData/tweets_small.json", "r")
 tweetData = json.load(tweetFile)
 tweetFile.close()
 
-# Continue your program below! 
+# Continue your program below!
+tweetText = [] 
+polarities = []
+subjectivities = []
+tweetstring = ''
+tweetBlob = TextBlob(tweetstring)
+
+for tweet in tweetData:
+    tweetstring += tweet["text"]
+    tweets = TextBlob(tweet["text"])
+    tweetText.append(tweets)
+    subjectivities.append(tweets.subjectivity)
+    polarities.append(tweets.polarity)
+
+wordDict = {}
+
+for word in tweetBlob.words:
+    wordDict[word.lower()]=tweetBlob.word_counts[word.lower()]
+
+avgPolarity = (sum(polarities)/len(polarities))
+avgSubjectivity = sum(subjectivities)/len(subjectivities)
+print(tweetstring)
+#print(avgPolarity)
+#print(avgSubjectivity)
+
 
 # Textblob sample:
-tb = TextBlob("You are a brilliant computer scientist.")
-print(tb.polarity)
+# tb = TextBlob("You are a brilliant computer scientist.")
+# print(tb.sentiment)
+
+plt.hist(polarities, bins=[-1, -0.9, -0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1, 1.1, 1.2, 1.3, 1.4, 1.5])
+plt.xlabel('Values')
+plt.ylabel('Number of Items')
+plt.title('Histogram of Polarities')
+plt.axis([-0.7, 1.5, 0, 50])
+plt.grid(True)
+plt.show()
+
